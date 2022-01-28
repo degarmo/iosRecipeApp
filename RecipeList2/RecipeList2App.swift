bc7dfecd3d67e03a322087957cd84e0bf20fffdb
Cory DeGarmo <degarmo@gmail.com>
2022-01-27 20:12:05 -0600

    Initial Commit

---
+++ RecipeList2/RecipeList2App.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeList2App.swift
+//  RecipeList2
+//
+//  Created by CD on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
