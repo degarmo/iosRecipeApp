bb997ec919fd0b873832806b6d5c5bc5c7bc6a00
Cory DeGarmo <degarmo@gmail.com>
2022-03-08 19:06:30 -0600

    added lazy stack and changed the view of the recipe list.

--- RecipeList2/Views/RecipeListView.swift
+++ RecipeList2/Views/RecipeListView.swift
@@ -14,27 +14,41 @@ struct RecipeListView: View {
     var body: some View {
         
         NavigationView {
-            List(model.recipes) { r in
-                NavigationLink(
-                    destination: RecipeDetailView(recipe: r),
-                    label: {
-                HStack {
-                    Image(r.image)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 50, height: 50, alignment: .center)
-                        .clipped()
-                        .cornerRadius(1)
-                    Text(r.name)
+            VStack(alignment: .leading){
+                Text("All Recipes")
+                    .bold()
+                    .font(.largeTitle)
+                
+                ScrollView {
+                    LazyVStack (alignment: .leading){
+                        ForEach(model.recipes) { r in
+                            NavigationLink(
+                                destination: RecipeDetailView(recipe: r),
+                                label: {
+                                    HStack {
+                                        Image(r.image)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 50, height: 50, alignment: .center)
+                                            .clipped()
+                                            .cornerRadius(1)
+                                        Text(r.name)
+                                            .foregroundColor(.black)
+                                    }
+                                })
+                        }
                     }
-                })
-            }.navigationBarTitle("All Recipes")
-       }
+                }
+            }
+            .navigationBarHidden(true)
+            .padding(.leading)
+        }
     }
 }
 
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
